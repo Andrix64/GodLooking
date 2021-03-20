25495f0fd2298c796950d4c5f4df25a225225e01
Andrea Lozza <44907638+Andrix64@users.noreply.github.com>
2021-03-20 11:08:23 +0100

    a

--- TEST/TEST_2.0/class/modo.js
+++ TEST/TEST_2.0/class/modo.js
@@ -1,17 +1,49 @@
+//importa three.js
+import * as THREE from '../../../three.js/build/three.module.js';
+
+//importa i controlli orbitali
+import { OrbitControls } from '../../../three.js/examples/jsm/controls/OrbitControls.js';
+
+//importa i loader di modelli,texture e decompressori
+import { GLTFLoader } from '../../../three.js/examples/jsm/loaders/GLTFLoader.js';
+import { MTLLoader } from '../../../three.js/examples/jsm/loaders/MTLLoader.js';
+import { EXRLoader } from '../../../three.js/examples/jsm/loaders/EXRLoader.js';
+import { DRACOLoader } from '../../../three.js/examples/jsm/loaders/DRACOLoader.js';
+
+//importa il mapper dei riflessi hdr
+import { RoughnessMipmapper } from '../../../three.js/examples/jsm/utils/RoughnessMipmapper.js';
+
+//Importa necessario per post processing
+import { EffectComposer } from '../../../three.js/examples/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from '../../../three.js/examples/jsm/postprocessing/RenderPass.js';
+import { BokehPass } from '../../../three.js/examples/jsm/postprocessing/BokehPass.js';
+import { SAOPass } from '../../../three.js/examples/jsm/postprocessing/SAOPass.js';
+
+
+
+
+
+
 class Mondo {
     constructor(container) {
-        this.container = container;//const container = document.querySelector('#scene-container');
-    }
-
-    initScene() {
+        this.container = container;
+        this.postprocessing = {};
         this.camera= new THREE.PerspectiveCamera( 40, 1,0.01, 1000 );
         this.camera.position.set(-1,0,1);
+
+        
+
+        this.dracoLoader = new DRACOLoader();
+        this.dracoLoader.setDecoderPath( '../../../three.js/examples/js/libs/draco/' );
 
         this.scene = new THREE.Scene();
         {
             const color = 'lightblue';
-            scene.background = new THREE.Color(color);
+            this.scene.background = new THREE.Color(color);
         }
+
+        this.mixer = new THREE.AnimationMixer( this.scene );
+
         this.clock = new THREE.Clock();
 
         this.renderer = new THREE.WebGLRenderer( { antialias: true } );
@@ -25,5 +57,116 @@ class Mondo {
         this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;//SHADOWMAP TYPE
 
         this.container.appendChild( this.renderer.domElement );
+
+        this.initPostprocessing()
+        this.initScene()
+    }
+
+    async initScene() {
+        const paths=["../gltf/Cittadino/Cittadina.gltf","../gltf/LowPolyTotem/Totem.gltf"]//oggetti da caricare
+
+        const requests = paths.map((path) => { 
+            return this.LoadObject(path) // Async function to send the mail.
+        })
+        const objects= await Promise.all(requests);
+        console.log(objects);
+        for(var i=0;i<objects.length;i++){
+            this.scene.add(objects[i].scene);
+        }
+
+        this.renderer.setSize( window.innerWidth, window.innerHeight );
+        this.postprocessing.composer.setSize( window.innerWidth, window.innerHeight );
+        this.render();
+    }
+
+    initPostprocessing() {
+        const renderPass = new RenderPass( this.scene, this.camera );
+    
+        const bokehPass = new BokehPass( this.scene, this.camera, {
+            focus: 1.3,
+            aperture: 0.0005,
+            maxblur: 0.015,
+    
+            width: window.innerWidth, 
+            height: window.innerHeight
+        } );
+        const saoPass = new SAOPass( this.scene, this.camera, false, true );
+        saoPass.params['output']=SAOPass.OUTPUT.Default;
+        saoPass.params['saoBias']=1;
+        saoPass.params['saoIntensity']=0.002;
+        saoPass.params['saoScale']=1.5;
+        saoPass.params['saoKernelRadius']=49;
+        saoPass.params['saoMinResolution']=0;
+        saoPass.params['saoBlur']=true;
+        saoPass.params['saoBlurRadius']=53.8;
+        const composer = new EffectComposer( this.renderer );
+    
+        composer.addPass( renderPass );
+        composer.addPass( saoPass );
+        composer.addPass( bokehPass );
+        
+    
+        this.postprocessing.composer = composer;
+        this.postprocessing.bokeh = bokehPass;
+        this.postprocessing.saoPass =saoPass
+    }
+
+    LoadEquirectangular(path){
+
+        new EXRLoader()
+        .setDataType( THREE.UnsignedByteType )
+        .load( '../hdr/suburban_parking_area_2k.exr', function ( texture ) {
+            const envMap = pmremGenerator.fromEquirectangular( texture ).texture;
+            const rt = new THREE.WebGLCubeRenderTarget(texture.image.height);
+              rt.fromEquirectangularTexture(renderer, texture);
+            scene.background = rt;
+
+            scene.environment = envMap;
+
+            texture.dispose();
+            pmremGenerator.dispose();
+            allRendered();
+
+        },
+        function ( xhr ) {
+            document.getElementById("load3").innerHTML="EXR: "+Math.floor( xhr.loaded / xhr.total * 100 )+"% loaded";
+        },
+        function ( error ) {
+            document.getElementById("load3").innerHTML="An error happened loading EXR";
+
+        } );
+        
+    }
+
+    async LoadObject(path){
+
+        const loader = new GLTFLoader();
+        loader.setDRACOLoader( this.dracoLoader );
+    
+        const object = await Promise.all([
+            loader.loadAsync(path),
+        ]);
+        console.log(object[0]);
+    
+        
+        object[0].animations.forEach( ( clip ) => {
+    
+            this.mixer.clipAction( clip ).play();
+    
+        } );
+        
+    
+        return object[0];
+    }
+
+    render() {
+        var delta = this.clock.getDelta();
+        if ( this.mixer ) this.mixer.update( delta );
+
+        this.postprocessing.composer.render( delta );
     }
 }
+
+
+
+export { Mondo };
--- TEST/TEST_2.0/main.js
+++ TEST/TEST_2.0/main.js
@@ -1,3 +1,7 @@
+import { Mondo } from './class/modo.js';
+const container = document.querySelector('#scene-container');
+const world = new Mondo(container);
+/*
 //importa three.js
 import * as THREE from '../../three.js/build/three.module.js';
 
@@ -27,7 +31,7 @@ let allrend=0;
 let materialLogo;
 let dimensionLogo;
 let mixer,clock;
-const postprocessing = {};
+
 
 //configurazione del color-picker
 $('#color-picker').spectrum({
@@ -90,78 +94,17 @@ function changeLogo(){
 //Funzione principale
 function init() {
     //posizionamento della finestra di render nella pagina
-    const container = document.getElementById( 'scene-container' );
 
     //Clock per animazione
-    clock = new THREE.Clock();
+    
 
     //posizione camera
-    camera = new THREE.PerspectiveCamera( 40, 1,0.01, 1000 );
-    camera.position.set(-1,0,1);
 
     //instanziamento Scena
-    scene = new THREE.Scene();
-    {
-        //Nebbia
-        /*const near = 200;
-        const far = 1200;*/
-        const color = 'lightblue';
-        //scene.fog = new THREE.Fog(color, near, far);
-        scene.background = new THREE.Color(color);
-    }
+    
 
     // Caricamento DracoLoader per la decompressione del file
-    const dracoLoader = new DRACOLoader();
-    dracoLoader.setDecoderPath( '../../three.js/examples/js/libs/draco/' );
-
-
-    // Caricamento Scena
-    const loader = new GLTFLoader();
-    loader.setDRACOLoader( dracoLoader );
-    loader.load('../gltf/Cittadino/Cittadina.gltf',
-        function ( gltf ) {
-            document.getElementById("load1").innerHTML="Scena: 100% loaded";
-            gltf.scene.scale.multiplyScalar(1);
-
-            const roughnessMipmapper = new RoughnessMipmapper( renderer );
-            scene.add( gltf.scene );
-            gltf.scene.traverse( function ( child ) {
-                if ( child.isMesh ) {
-                    roughnessMipmapper.generateMipmaps( child.material );
-                    child.castShadow = true;
-                    child.receiveShadow = true;
-                    sceneMeshes.push(child);
-                    if(child.name=="Glass")
-                    {
-                        child.material.transparent=true;
-                        child.material.opacity=0.4;
-                    }
-                }
-            } );
-        
-        
-            roughnessMipmapper.dispose();
-
-            //carica Animazione
-            mixer = new THREE.AnimationMixer( gltf.scene );
-
-            gltf.animations.forEach( ( clip ) => {
-
-                mixer.clipAction( clip ).play();
-
-            } );
-
-            //renderizza la scena
-            allRendered();
-    },
-        function ( xhr ) {
-            document.getElementById("load1").innerHTML="Scena: "+Math.floor( xhr.loaded / xhr.total * 50 )+"% loaded";
-        },
-        function ( error ) {
-            document.getElementById("load1").innerHTML="An error happened loading the scene";
-
-        }
-    );
+    
 
 
     // Caricamento Oggetto
@@ -228,46 +171,13 @@ function init() {
 
     
     // Caricamento EXR
-    new EXRLoader()
-        .setDataType( THREE.UnsignedByteType )
-        .setPath( '../hdr/' )
-        .load( 'suburban_parking_area_2k.exr', function ( texture ) {
-            const envMap = pmremGenerator.fromEquirectangular( texture ).texture;
-            const rt = new THREE.WebGLCubeRenderTarget(texture.image.height);
-              rt.fromEquirectangularTexture(renderer, texture);
-            scene.background = rt;
-
-            scene.environment = envMap;
-
-            texture.dispose();
-            pmremGenerator.dispose();
-            allRendered();
-
-        },
-        function ( xhr ) {
-            document.getElementById("load3").innerHTML="EXR: "+Math.floor( xhr.loaded / xhr.total * 100 )+"% loaded";
-        },
-        function ( error ) {
-            document.getElementById("load3").innerHTML="An error happened loading EXR";
-
-        } );
 
 
     //Settings del render
-    renderer = new THREE.WebGLRenderer( { antialias: true } );
-    renderer.setPixelRatio( window.devicePixelRatio );
-
-    renderer.toneMapping = THREE.ACESFilmicToneMapping;
-    renderer.toneMappingExposure = 1;
-    renderer.outputEncoding = THREE.sRGBEncoding;
-
-    renderer.shadowMap.enabled = false;//SHADOWMAP
-    renderer.shadowMap.type = THREE.PCFSoftShadowMap;//SHADOWMAP TYPE
-
-    container.appendChild( renderer.domElement );
+    
 
     // Creazione composer per post-processing
-    initPostprocessing()
+    
 
     //instinziamento della mappa di riflessi hdr
     pmremGenerator = new THREE.PMREMGenerator( renderer );
@@ -339,37 +249,7 @@ function init() {
 init();
 
 //init del postprocessing
-function initPostprocessing() {
-    const renderPass = new RenderPass( scene, camera );
 
-    const bokehPass = new BokehPass( scene, camera, {
-        focus: 1.3,
-        aperture: 0.0005,
-        maxblur: 0.015,
-
-        width: window.innerWidth, 
-        height: window.innerHeight
-    } );
-    const saoPass = new SAOPass( scene, camera, false, true );
-    saoPass.params['output']=SAOPass.OUTPUT.Default;
-    saoPass.params['saoBias']=1;
-    saoPass.params['saoIntensity']=0.002;
-    saoPass.params['saoScale']=1.5;
-    saoPass.params['saoKernelRadius']=49;
-    saoPass.params['saoMinResolution']=0;
-    saoPass.params['saoBlur']=true;
-    saoPass.params['saoBlurRadius']=53.8;
-    const composer = new EffectComposer( renderer );
-
-    composer.addPass( renderPass );
-    composer.addPass( saoPass );
-    composer.addPass( bokehPass );
-    
-
-    postprocessing.composer = composer;
-    postprocessing.bokeh = bokehPass;
-    postprocessing.saoPass =saoPass
-}
 
 //funzione per la scomparsa della schermata di caricamento
 function fadeout(){
@@ -454,3 +334,4 @@ function render() {
 
     postprocessing.composer.render( delta );
 }
+*/
